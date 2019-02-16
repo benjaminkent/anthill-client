298c487f358a9d91ba19eefbf00f5b2f4c2cbf46
benjaminkent <benjamin.k.jehl@gmail.com>
2019-02-16 00:10:57 -0500

    header component added

--- src/App.vue
+++ src/App.vue
@@ -18,5 +18,7 @@
 </script>
 
 <style lang="scss">
-
+  body {
+    margin: 0;
+  }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,78 @@
 <template>
-  <div>taco tuesday</div>
+  <div class="header">
+    <header class="header">
+      <img class="logo" src="../assets/ant-logo.svg" alt="logo">
+      <nav class="big-nav">
+        <ul class="nav-list">
+          <li class="big-header">
+            <a href="#">Home</a>
+          </li>
+          <li class="big-header">
+            <a href="#">About</a>
+          </li>
+          <li class="big-header">
+            <a href="#">Meet the squad</a>
+          </li>
+          <li class="hamburger"></li>
+        </ul>
+      </nav>
+    </header>
+  </div>
 </template>
 
 <script>
 export default {
   name: "Header"
-}
+};
 </script>
+
+<style scoped lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Major+Mono+Display");
+
+.header {
+  position: fixed;
+  width: 100%;
+  background-color: black;
+  color: white;
+  box-shadow: 0px 0.5px 5px black;
+  z-index: 2;
+  font-family: "Major Mono Display", monospace;
+  display: flex;
+  justify-content: space-between;
+}
+
+.nav-list {
+  list-style-type: none;
+  display: flex;
+  margin: 2.5rem 0 0 0;
+  padding: 0;
+  justify-content: space-around;
+
+  a {
+    color: white;
+    text-decoration: none;
+    font-size: 1.3rem;
+  }
+}
+
+.big-header {
+  margin: 0 0.5rem;
+}
+
+.logo {
+  height: 2.5rem;
+  margin: 2rem 0 1.5rem 0.5rem;
+}
+
+@media (max-width: 501px) {
+  .big-header {
+    display: none;
+  }
+}
+
+@media (min-width: 600px) {
+  .big-header {
+    margin: 0 1.7rem;
+  }
+}
+</style>
